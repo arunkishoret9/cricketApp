705c053e456238552d5c036642cb591e55fe3b65
00877117 <arun.kishore@aa.com>
2018-10-06 22:01:44 -0500

    updated Controller class

---
+++ src/main/java/controllers/Team.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import Models.PlayersInfo;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Team {
+
+    @ApiOperation("This API return the player information")
+    @ApiResponse(code = 200, message = "Successfull")
+    @RequestMapping(value = "v1/playersInfo", produces = {"application/json"}, method = RequestMethod.GET)
+    public ResponseEntity<PlayersInfo> getPlayerDetails(
+            @ApiParam(value="Player name", required = true) @RequestParam(value = "Player name", required = true) String playerName
+    ){
+        PlayersInfo playersInfo = new PlayersInfo();
+        return new ResponseEntity<>(playersInfo, HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/com/cricket/tournament/cricket/CricketApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cricket.tournament.cricket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class CricketApplicationTests {
+
+    @Test
+    public void test(){
+
+    }
+}
